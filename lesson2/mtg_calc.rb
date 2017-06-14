9f899dfa2f6c7d587603a631afbeb93e4202691b
Annie Liu <annieliu38@gmail.com>
2017-06-14 15:32:09 +0000

    update to mtg calc

--- lesson2/mtg_calc.rb
+++ lesson2/mtg_calc.rb
@@ -7,37 +7,37 @@ def prompt(msg)
   Kernel.puts ">> #{msg}"
 end
 
-def valid_number?(amt)
-  (amt.to_i.to_s == amt || amt.to_f.to_s == amt) && amt.to_f >= 0
+def valid_number?(str_amt)
+  (str_amt.to_i.to_s == str_amt) ||
+    (format("%.2f", str_amt.to_f.to_s) == str_amt) &&
+      (str_amt.to_f > 0)
+end
+
+def ask_for(mtg_info, mtg_info_var)
+  loop do
+    prompt("Please enter the #{mtg_info}:")
+    mtg_info_var = Kernel.gets().chomp()
+    break if valid_number?(mtg_info_var)
+    prompt(MESSAGES["valid num"])
+  end
+  mtg_info_var
 end
 
 prompt(MESSAGES['welcome'])
 
 # largest loop starts here
 loop do
+  # obtain input from user (3x)
   loan_amount = ''
-  loop do
-    prompt(MESSAGES["loan_amount"])
-    loan_amount = Kernel.gets().chomp()
-    break if valid_number?(loan_amount)
-    prompt(MESSAGES["valid_num"])
-  end
+  loan_amount = ask_for("loan amount", loan_amount)
 
   annual_percentage_rate = ''
-  loop do
-    prompt(MESSAGES["apr"])
-    annual_percentage_rate = Kernel.gets().chomp()
-    break if valid_number?(annual_percentage_rate)
-    prompt(MESSAGES["valid_num"])
-  end
+  annual_percentage_rate = ask_for("annual percentage rate (APR)",
+                                   annual_percentage_rate)
 
   loan_duration_in_years = ''
-  loop do
-    prompt(MESSAGES["loan_duration_in_years"])
-    loan_duration_in_years = Kernel.gets().chomp()
-    break if valid_number?(loan_duration_in_years)
-    prompt(MESSAGES["valid_num"])
-  end
+  loan_duration_in_years = ask_for("loan duration in years",
+                                   loan_duration_in_years)
 
   # calculations:
   monthly_interest_rate = (annual_percentage_rate.to_f / 12) / 100
@@ -51,9 +51,17 @@ loop do
 
   # output to user:
   prompt("Your monthly payment will be $#{monthly_payment}")
+
   prompt(MESSAGES["again"])
-  answer = Kernel.gets().chomp().downcase
-  break unless answer.start_with?("y")
+  again = Kernel.gets().chomp().downcase
+  if again == ("n" || "no")
+    break
+  elsif again == ("y" || "yes")
+    next
+  else
+    prompt("Not sure what you meant")
+    break
+  end
 
   system('clear') || system('cls')
 end
